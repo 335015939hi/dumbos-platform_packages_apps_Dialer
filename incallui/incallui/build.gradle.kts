cd42e04478962b6e568ed3036286068eb284134c
Charles Lombardo <clombardo169@gmail.com>
2025-01-24 16:43:39 -0500

    Refactor com.android.incallui to :incallui:incallui
    Additionally includes com.android.dialer.feedback

---
+++ incallui/incallui/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(libs.versions.java.get().toInt()))
+    }
+}
+
+android {
+    namespace = "com.android.incallui"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+    }
+
+    sourceSets.getByName("main") {
+        java.exclude(
+            "**/calllocation/impl/**",
+            "**/maps/impl/**"
+        )
+    }
+
+    lint {
+        abortOnError = false
+    }
+}
+
+dependencies {
+    implementation(libs.design)
+    implementation(libs.support.v4)
+    implementation(libs.appcompat.v7)
+
+    implementation(libs.dagger)
+    annotationProcessor(libs.dagger.compiler)
+
+    compileOnly(libs.auto.value.annotations)
+    annotationProcessor(libs.auto.value)
+
+    implementation(libs.guava)
+
+    implementation(libs.protobuf.java)
+
+    // Using an old version because they migrated to androidx in 4.10.0 which breaks building with
+    // the android support libraries
+    // TODO: Migrate to Androidx and update Glide
+    implementation(libs.glide)
+
+    implementation(project(":contacts-common"))
+    implementation(project(":protos"))
+    implementation(project(":dialer:common"))
+    implementation(project(":dialer:resources"))
+    implementation(project(":bubble"))
+
+    implementation(project(":lib:platform_frameworks_ex:common"))
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,6 +24,7 @@ include(":quantum")
 include(":product")
 
 include(":incallui:resources")
+include(":incallui:incallui")
 
 include(":contacts-common")
 
